8329b81d3fb3eac0fb2263d1f4a806afd135d87f
Ritu <ritu1994235@gmail.com>
2018-04-24 14:57:35 -0400

    Preprocessed and parsed Data files

---
+++ Train.py
@@ -0,0 +1,14 @@
+from tensorflow.contrib import learn
+import numpy as np
+import ReadData as read
+
+print("Loading data...")
+x_text, y = read.load_data()
+
+
+max_document_length = max([len(x.split(" ")) for x in x_text])
+vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
+x = np.array(list(vocab_processor.fit_transform(x_text)))
+
+
+print (max_document_length)
